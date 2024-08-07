17e88b868fb59d2acbe17e7ed96890acdabedb77
Alana IFSC <alana.brm@aluno.ifsc.edu.br>
2024-08-06 22:16:43 -0300

    Em andamento

--- src/routes/locais.routes.js
+++ src/routes/locais.routes.js
@@ -1,6 +1,14 @@
 const { Router } = require('express')
 
+const LocaisController = require('../controllers/local.controller')
+
+
 const locaisRoutes = Router()
 
+locaisRoutes.post('/', LocaisController.create);
+locaisRoutes.get('/', LocaisController.read);
+locaisRoutes.get('/:local_id', LocaisController.readOne);
+locaisRoutes.put('/:local_id', LocaisController.update);
+locaisRoutes.delete('/:local_id', LocaisController.delete);
 
 module.exports = locaisRoutes
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,7 +1,11 @@
 const { Router } = require('express');
 
+const UsuarioController = require('../controllers/usuario.controller');
+
 
 const usuariosRoutes = new Router();
+
+usuariosRoutes.post('/', UsuarioController.create);
 
 
 module.exports = usuariosRoutes;
